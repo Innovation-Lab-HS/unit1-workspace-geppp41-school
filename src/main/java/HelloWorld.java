983a3b07a7ef7e16e86acbbe0fdd18bdcb1de991
geppp41-school <1118807@apps.nsd.org>
2024-09-13 11:18:09 -0700

    added an error print to HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void say_hello(String text) {
         System.out.println("Hello World!");
         System.out.println(text);
+        System.err.println("there is no error");
     }
 }
