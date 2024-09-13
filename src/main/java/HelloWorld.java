04d6f4d9e2cc57867674a53e022ddff683350a51
geppp41-school <1118807@apps.nsd.org>
2024-09-13 11:03:10 -0700

    created Main.java edited HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
 package src.main.java;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void say_hello(String text) {
         System.out.println("Hello World!");
+        System.out.println(text);
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+package src.main.java;
+
+import src.main.java.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld.say_hello("Hello!");
+    }
+}
